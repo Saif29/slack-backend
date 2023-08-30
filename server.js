23beac1d12e9b473571189642b61dccf565f883a
Saif Ur Rehman <58169470+Saif29@users.noreply.github.com>
2023-08-30 11:06:43 +0500

    Update server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const PORT = process.env.PORT || 3050;
 const io = require("socket.io")(server, {
     cors: {
         origin: "https://slackclone1.netlify.app",
-        //origin: "http://localhost:3000",
+        // origin: "http://localhost:3000",
         methods: ["GET", "POST"],
     },
 });
